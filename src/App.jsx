32a8758ec51ce63f096155d5fbadd1ecbda29c84
sumanth-botlagunta <sumanthbotlagunta977@gmail.com>
2023-07-14 12:07:42 +0530

    react hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,16 @@
 import React from 'react';
-import { createRoot } from 'react-dom';
-
-const Pet = (props) => {
-  return React.createElement('div', {}, [
-    React.createElement('h1', {}, props.name),
-    React.createElement('h1', {}, props.animal),
-    React.createElement('h1', {}, props.breed),
-  ]);
-};
+import { createRoot } from 'react-dom/client';
+import SearchParams from './SearchParams';
 
 const App = () => {
-  return React.createElement(
-    'div',
-    {},
-    React.createElement('h1', {}, 'Adopt Me!!'),
-    React.createElement(Pet, {
-      animal: 'Dog',
-      name: 'Luna',
-      breed: 'havanese',
-    }),
-    React.createElement(Pet, {
-      animal: 'Parrot',
-      name: 'totha',
-      breed: 'green',
-    }),
-    React.createElement(Pet, {
-      animal: 'Cat',
-      name: 'kitty',
-      breed: 'whiter',
-    })
+  return (
+    <div>
+      <h1>Adopt Me!!</h1>
+      <SearchParams />
+    </div>
   );
 };
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
